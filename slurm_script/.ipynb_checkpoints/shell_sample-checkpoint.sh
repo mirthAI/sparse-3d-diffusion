c0533904f8d2af3e62b6ef193f96d1b38da82c7d
Sebastianjhx <sebastianjhx@163.com>
2026-04-20 00:48:40 -0400

    Files Uploaded

--- slurm_script/.ipynb_checkpoints/shell_sample-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-module load conda
-conda activate hxj_b200
-
-export OMP_NUM_THREADS=8
-
-torchrun --standalone --nproc_per_node=2 super_res_sample_3d.py --config="configs/ps128_pet.yml" --model_path="/red/weishao/weishao/DPM_3D/output_PET/ckpt_fast_ts5_x0_ch64_s222_pet/ema_0.99_200000.pt"
